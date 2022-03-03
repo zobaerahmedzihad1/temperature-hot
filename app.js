8144f08cf0012e8e1b6a59c1ea8f39ae2f1ec1a1
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-03 10:03:04 +0600

    temperature hot api project complete.

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const API_KEY = `9a640d6191481e15e612c52f4cc41439`;
 // search city by name
 const search_city = document.getElementById("search_city");
 // error handling
-const error = document.getElementById("error");
+const error1 = document.getElementById("error");
+const error2 = document.getElementById("error2");
+// error2.style.backgroundColor = "white"
 // content
 const content = document.getElementById("content");
 
 const searchWeather = () => {
   // search city by name
   const city_name = search_city.value;
-  search_city.value = "";
 
   if (city_name == "") {
-    error.style.display = "block";
+    error2.innerText = "";
+    error1.style.display = "block";
     content.style.display = "none";
   } else {
-    error.style.display = "none";
+    error1.style.display = "none";
     content.style.display = "block";
+    error2.innerText = "";
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`;
     fetch(url)
       .then((response) => response.json())
@@ -31,14 +35,19 @@ const setInnerText = (id, text) => {
 
 // set data from api to website
 const searchTemperature = (temperature) => {
-  // if(temperature == )
+  if (temperature.name) {
+    setInnerText("city_name", temperature.name);
+    setInnerText("temperature", temperature.main.temp);
+    setInnerText("clouds", temperature.weather[0].main);
 
-  setInnerText("city_name", temperature.name);
-  setInnerText("temperature", temperature.main.temp);
-  setInnerText("clouds", temperature.weather[0].main);
-
-  //   dynamically set weather icon
-  const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-  const icon = document.getElementById("icon");
-  icon.setAttribute("src", url);
+    //   dynamically set weather icon
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const icon = document.getElementById("icon");
+    icon.setAttribute("src", url);
+  } else {
+    content.style.display = "none";
+    error1.style.display = "none";
+    error2.innerText = `" ${search_city.value} " this name is not a valid city name.`;
+  }
+  search_city.value = "";
 };
